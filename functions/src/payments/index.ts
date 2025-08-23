33637c57c8a918e0b3adb592c4e2b7666c43f15f
Serge Amouzou <serge@tryverza.com>
2025-08-23 00:56:19 +0000

    lint fixes

--- functions/src/payments/index.ts
+++ functions/src/payments/index.ts
@@ -1,5 +1,5 @@
 
-import {onCall, onRequest} from "firebase-functions/v2/https";
+import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import Stripe from "stripe";
 import {db} from "../config/firebase";
