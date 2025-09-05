58b37d2e4482acf52ef9c8e042cc16947186d11f
Serge Amouzou <serge@tryverza.com>
2025-09-05 18:28:34 +0000

    ok, remember a while back we had a $25/month subscription for creators a

--- src/types/index.ts
+++ src/types/index.ts
@@ -177,7 +177,7 @@ export interface UserProfileFirestoreData {
   stripeCustomerId?: string | null;
   stripeSubscriptionId?: string | null;
   subscriptionStatus?: 'trialing' | 'active' | 'past_due' | 'canceled' | 'incomplete' | 'unpaid' | 'paused' | 'none' | 'incomplete_expired';
-  subscriptionPlanId?: 'individual_free' | 'agency_start_monthly' | 'agency_start_yearly' | 'agency_pro_monthly' | 'agency_pro_yearly';
+  subscriptionPlanId?: 'individual_free' | 'individual_monthly' | 'individual_yearly' | 'agency_start_monthly' | 'agency_start_yearly' | 'agency_pro_monthly' | 'agency_pro_yearly';
   talentLimit?: number; // Talent limit for agency plans
   subscriptionInterval?: 'day' | 'week' | 'month' | 'year' | null;
   trialEndsAt?: ClientTimestamp | null;
