e3a1932c54b61a787f769c1a33673df98d2e361f
Serge Amouzou <serge@tryverza.com>
2025-09-16 17:17:19 +0000

    do you see how the syncfusing icons look like they lack styling? what's

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,15 @@
 import type { Metadata } from 'next';
 import { GeistSans } from 'geist/font/sans';
 import './globals.css';
-import '@syncfusion/ej2-react-documenteditor/styles/tailwind.css'; // Use Tailwind theme
+import '@syncfusion/ej2-base/styles/material.css'; // Use a complete theme
+import '@syncfusion/ej2-buttons/styles/material.css';
+import '@syncfusion/ej2-dropdowns/styles/material.css';
+import '@syncfusion/ej2-inputs/styles/material.css';
+import '@syncfusion/ej2-lists/styles/material.css';
+import '@syncfusion/ej2-navigations/styles/material.css';
+import '@syncfusion/ej2-popups/styles/material.css';
+import '@syncfusion/ej2-splitbuttons/styles/material.css';
+import '@syncfusion/ej2-react-documenteditor/styles/material.css';
 import { Toaster } from '@/components/ui/toaster';
 import { AuthProvider } from '@/hooks/use-auth'; 
 import { AuthGuard } from '@/components/auth-gaurd';
