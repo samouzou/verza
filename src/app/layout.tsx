65f169619f657cc9084f41a0029ac9d7fc6880a9
Serge Amouzou <serge@tryverza.com>
2025-09-17 18:37:17 +0000

    remember we had tailwind.css and realize it wasn't working with nextjs a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,15 @@
 import type { Metadata } from 'next';
 import { GeistSans } from 'geist/font/sans';
 import './globals.css';
-import '@syncfusion/ej2-base/styles/material.css'; // Use a complete theme
-import '@syncfusion/ej2-buttons/styles/material.css';
-import '@syncfusion/ej2-dropdowns/styles/material.css';
-import '@syncfusion/ej2-inputs/styles/material.css';
-import '@syncfusion/ej2-lists/styles/material.css';
-import '@syncfusion/ej2-navigations/styles/material.css';
-import '@syncfusion/ej2-popups/styles/material.css';
-import '@syncfusion/ej2-splitbuttons/styles/material.css';
-import '@syncfusion/ej2-react-documenteditor/styles/material.css';
+import '@syncfusion/ej2-base/styles/tailwind.css';
+import '@syncfusion/ej2-buttons/styles/tailwind.css';
+import '@syncfusion/ej2-dropdowns/styles/tailwind.css';
+import '@syncfusion/ej2-inputs/styles/tailwind.css';
+import '@syncfusion/ej2-lists/styles/tailwind.css';
+import '@syncfusion/ej2-navigations/styles/tailwind.css';
+import '@syncfusion/ej2-popups/styles/tailwind.css';
+import '@syncfusion/ej2-splitbuttons/styles/tailwind.css';
+import '@syncfusion/ej2-react-documenteditor/styles/tailwind.css';
 import { Toaster } from '@/components/ui/toaster';
 import { AuthProvider } from '@/hooks/use-auth'; 
 import { AuthGuard } from '@/components/auth-gaurd';
