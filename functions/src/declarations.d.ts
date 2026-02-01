50ad91b8b3c93ea278d0cde1cf737df2ea9b7565
Serge Amouzou <serge@tryverza.com>
2026-02-01 02:16:17 +0000

    ok, i think we need to update the security tules for the user to be able

--- functions/src/declarations.d.ts
+++ functions/src/declarations.d.ts
@@ -1,7 +1,7 @@
 // This declaration file is necessary to address a specific import path used by a Genkit dependency (dotprompt).
 // It tells TypeScript to use the main 'handlebars' type definitions when it encounters
 // an import from 'handlebars/dist/cjs/handlebars.js', which resolves the build error.
-declare module 'handlebars/dist/cjs/handlebars.js' {
-    import * as Handlebars from 'handlebars';
+declare module "handlebars/dist/cjs/handlebars.js" {
+    import * as Handlebars from "handlebars";
     export = Handlebars;
 }
