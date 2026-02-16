dad4a96b711b477960f59264d43c342bb74dcc83
Serge Amouzou <serge@tryverza.com>
2026-02-16 02:10:50 +0000

    i'm getting this ts error: Type 'Promise<void>' is not assignable to typ

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,10 +3,11 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 import { enableFirebaseTelemetry } from '@genkit-ai/firebase';
 
+enableFirebaseTelemetry();
+
 export const ai = genkit({
   plugins: [
     googleAI(),
-    enableFirebaseTelemetry(),
   ],
-  model: googleAI.model('gemini-3-flash-preview'),
+  model: googleAI.model('gemini-1.5-flash-preview'),
 });
